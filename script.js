61cceae85915728943d84dc9f24626fdb605bd80
Júlio Davi <julio1704davi@gmail.com>
2025-11-29 21:25:33 -0300

    Parte do protótipo finalizada

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const header = document.querySelector("header");
 const menuBtn = document.querySelector("#menu-btn");
 const tiposCarros = document.querySelectorAll(".tipo-carro");
 let animationDelay = 0;
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log(entry)
+            entry.target.classList.add('animated')
+        } else {
+            entry.target.classList.remove('animated')
+            entry.target.classList.add('hide')
+        }
+    })
+}, {
+    threshold: 0.1
+})
+
+
 tiposCarros.forEach((tipo) => {
     tipo.style.animationDelay = `${animationDelay}s`;
     animationDelay += 0.1;
+    observer.observe(tipo);
 });
 
 function toggleMenu() {
